6e52b578bf8adc3f880ab77015cac70123312944
Joe Thorley <joe@poissonconsulting.ca>
2022-01-09 12:32:29 -0800

    - Added `if_else2()` with only two options.

---
+++ R/if_else2.R
@@ -0,0 +1,29 @@
+#' Vectorised if else.
+#' 
+#' Vectorised if else that if true returns first possibility otherwise returns
+#' second possibility (even if the condition is a missing value).
+#' 
+#' @inherit dplyr::if_else
+#' @return Where condition is `TRUE`, the matching value from `true`, where it's `FALSE` or `NA`, the matching value from `false`.
+#' @seealso [`ifelse()`] and [`dplyr::if_else()`].
+#' @export
+#' @examples 
+#' # consider the following data frame
+#' data <- tibble::tibble(
+#'   x = c(TRUE, FALSE, NA), 
+#'   y = c("x is false", NA, "hello"))
+#'   
+#' # with a single vector if_else2 behaves the same as the default call to if_else.
+#' data |>
+#'   dplyr::mutate(
+#'     y1 = dplyr::if_else(y != "x is false", "x is true", y),
+#'     y2 = dplyr::if_else(y != "x is false", "x is true", y))
+#' 
+#' # however in the case of a second vector if_else2 does not introduce missing values
+#' data |>
+#'   dplyr::mutate(
+#'     x1 = dplyr::if_else(y == "x is false", FALSE, x),
+#'     x2 = if_else2(y == "x is false", FALSE, x))
+if_else2 <- function(condition, true, false) {
+  dplyr::if_else(condition, true, false, missing = false)
+}
---
+++ tests/testthat/test-if-else2.R
@@ -0,0 +1,29 @@
+test_that("scalar true and false are vectorised", {
+  x <- c(TRUE, TRUE, FALSE, FALSE)
+  expect_identical(if_else2(x, 1, 2), c(1, 1, 2, 2))
+})
+
+test_that("vector true and false are ok", {
+  x <- c(-1, 0, 1)
+  
+  expect_identical(if_else2(x < 0, x, 0), c(-1, 0, 0))
+  expect_identical(if_else2(x > 0, x, 0), c(0, 0, 1))
+})
+
+test_that("missing values are preserved if x", {
+  x <- c(TRUE, NA, FALSE)
+  expect_identical(if_else2(x, FALSE, x), c(FALSE, NA, FALSE))
+})
+
+test_that("missing values are replaced", {
+  expect_identical(if_else2(c(TRUE, NA, FALSE), -1, 1), c(-1, 1, 1))
+})
+
+test_that("works with lists", {
+  x <- list(1, 2, 3)
+  
+  expect_equal(
+    if_else2(c(TRUE, TRUE, FALSE), x, list(NULL)),
+    list(1, 2, NULL)
+  )
+})

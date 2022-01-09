00f81d197f603f35f04076d2b507c50b1f906d73
Joe Thorley <joe@poissonconsulting.ca>
2022-01-09 12:40:54 -0800

    inherit string crush

--- R/str-crush.R
+++ R/str-crush.R
@@ -1,11 +1,12 @@
 #' Remove whitespace from a string
 #' 
-#' `str_crush()`, removes all whitespace from a string,
+#' `str_crush()`, which removes all whitespace from a string,
 #' is the logical extension to [`stringr::str_trim()`] and [`stringr::str_squish()`].
 #' 
 #' `str_crush()` is considered [too specialized](https://github.com/tidyverse/stringr/pull/338) to be part of stringr.
 #' 
-#' @param string A character vector.
+#' @inherit stringr::str_squish
+#' @seealso [`stringr::str_trim()`] and [`stringr::str_squish()`]
 #' @export
 #' @examples 
 #' str_crush("  String with trailing,  middle, and leading white space\t")

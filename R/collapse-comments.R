95889056980eb7c64d5523481157dbd0cce9a428
Joe Thorley <joe@poissonconsulting.ca>
2022-03-04 10:41:47 -0800

    - Added `collapse_comments()`.

---
+++ R/collapse-comments.R
@@ -0,0 +1,23 @@
+#' Collapse Comments
+#' 
+#' Collapse comments by concatenating, coercing to string
+#' and then collapsing into a single string (character scalar) 
+#' using the '. ' separator.
+#'
+#' @param ... objects to be collapsed into a string.
+#' @export
+#' @examples
+#' collapse_comments(c("Saw fish"), character(0), "Nice. .", NA_character_)
+collapse_comments <- function(...) {
+  x <- c(...)
+  x <- as.character(x)
+  if(length(x) == 0) return(character(0))
+  x <- x[!is.na(x)]
+  x <- x[nchar(x) > 0]
+  if(length(x) == 0) return("")
+  x <- stringr::str_replace(x, "(\\.\\s*)+$", "")
+  x <- stringr::str_squish(x)
+  sep <- ". "
+  x <- paste(x, sep = sep, collapse = sep)
+  paste(x, "", sep = ".")
+}

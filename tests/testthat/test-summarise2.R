07796490cc661add80f90b3ae57986eb400771a9
Ayla Pearson <ayla@poissonconsulting.ca>
2023-05-31 12:37:33 -0700

    adding tests

--- tests/testthat/test-summarise2.R
+++ tests/testthat/test-summarise2.R
@@ -4,13 +4,18 @@ test_that("multiple groups do not produce message", {
     id = c(1, 1, 2, 2),
     value = c(10, 4, 20, 6)
   )
+  output <- df |> dplyr::group_by(group, id) |> summarise2(mean = mean(value))
   expect_equal(
-    df |> dplyr::group_by(group, id) |> summarise2(mean = mean(value)),
+    output,
     data.frame(
       group = c("A", "B"),
       id = c(1, 2),
       mean = c(7, 13)
     ) |> dplyr::group_by(group, id)
+  )
+  expect_s3_class(
+    output,
+    c("grouped_df", "tbl_df", "tbl", "data.frame")
   )
 })
 
@@ -30,3 +35,21 @@ test_that("multiple groups do not produce message with summarize", {
   )
 })
 
+test_that("works on non grouped data", {
+  df <- data.frame(
+    group = c("A", "A", "B", "B"),
+    id = c(1, 1, 2, 2),
+    value = c(10, 4, 20, 6)
+  )
+  output <- df |> summarise2(mean = mean(value))
+  expect_equal(
+    output,
+    data.frame(
+      mean = c(10)
+    )
+  )
+  expect_s3_class(
+    output,
+    c("data.frame")
+  )
+})

236fca851706b6556a38e804a79ff109adb5cabd
Ayla Pearson <ayla@poissonconsulting.ca>
2023-05-31 14:56:09 -0700

    adding tests

--- tests/testthat/test-summarise2.R
+++ tests/testthat/test-summarise2.R
@@ -19,7 +19,7 @@ test_that("multiple groups do not produce message", {
   )
 })
 
-test_that("multiple groups do not produce message with summarize", {
+test_that("multiple groups do not produce message with summarize2", {
   df <- data.frame(
     group = c("A", "A", "B", "B"),
     id = c(1, 1, 2, 2),
@@ -51,5 +51,49 @@ test_that("works on non grouped data", {
   expect_s3_class(
     output,
     c("data.frame")
+  )
+})
+
+test_that("same data output as summarise with two groups", {
+  df <- data.frame(
+    group = c("A", "A", "B", "B"),
+    id = c(1, 1, 2, 2),
+    value = c(10, 4, 20, 6)
+  )
+  output <- df |> dplyr::group_by(group, id) |> summarise2(mean = mean(value))
+  orginal <- suppressMessages(
+    df |> dplyr::group_by(group, id) |> dplyr::summarise(mean = mean(value))
+  )
+  expect_equal(
+    output$group,
+    orginal$group
+  )
+  expect_equal(
+    output$id,
+    orginal$id
+  )
+  expect_equal(
+    output$mean,
+    orginal$mean
+  )
+})
+
+test_that("same data output as summarise with one group", {
+  df <- data.frame(
+    group = c("A", "A", "B", "B"),
+    id = c(1, 1, 2, 2),
+    value = c(10, 4, 20, 6)
+  )
+  output <- df |> dplyr::group_by(group) |> summarise2(mean = mean(value))
+  orginal <- suppressMessages(
+    df |> dplyr::group_by(group) |> dplyr::summarise(mean = mean(value))
+  )
+  expect_equal(
+    output$group,
+    orginal$group
+  )
+  expect_equal(
+    output$mean,
+    orginal$mean
   )
 })

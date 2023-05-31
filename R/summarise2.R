3413af03e709415029f22662125d1613349a460e
Ayla Pearson <ayla@poissonconsulting.ca>
2023-05-31 12:41:58 -0700

    adding example

--- R/summarise2.R
+++ R/summarise2.R
@@ -7,7 +7,12 @@
 #' @export
 #'
 #' @examples
-#' summarise2(mtcars, mean = mean(mpg))
+#'  df <- data.frame(
+#'   group = c("A", "A", "B", "B"), 
+#'   id = c(1, 1, 2, 2), 
+#'   value = c(10, 4, 20, 6)
+#'  )
+#' df |> dplyr::group_by(group, id) |> summarise2(mean = mean(value))
 summarise2 <- function(.data, ..., .by = NULL) {
   dplyr::summarize(.data, ..., .by = NULL, .groups = "keep")
 }

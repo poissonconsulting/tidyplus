91040e397fa4a40bacdc0c351f1e0ef09184e2c5
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-07-16 11:08:24 -0600

    removed `magrittr` dependencies resolves #4

--- R/duplicates.R
+++ R/duplicates.R
@@ -10,18 +10,18 @@ duplicates <- function(x, cols = colnames(x)) {
   check_values(cols, "")
   check_names(x, cols)
   
-  x %<>% tibble::as_tibble()
+  x <- tibble::as_tibble(x)
   
-  cols %<>% unique()
+  cols <- unique(cols)
   
   if (!length(cols)) {
     return(x)
   }
   
   y <- x[cols]
-  y <- y[duplicated(y), , drop = FALSE] %>%
-    unique()
-  x %<>% merge(y, by = cols)
-  x %<>% dplyr::as_tibble()
+  y <- y[duplicated(y), , drop = FALSE]
+  y <- unique(y)
+  x <- merge(x, y, by = cols)
+  x <- dplyr::as_tibble(x)
   x
 }

ab133ce84cba3bf5ad316b9a5ff3c162389e165e
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-07-16 21:46:12 -0600

    edited and added `duplicates()` tests

--- tests/testthat/test-duplicates.R
+++ tests/testthat/test-duplicates.R
@@ -1,4 +1,4 @@
-test_that("can read multiple hobo csv file", {
+test_that("returns only duplicated rows of selected columns", {
   tib <- dplyr::tibble(x = c(1, 2, 1), y = c(1, 1, 1))
   expect_identical(duplicates(tib), tib[c(1, 3), ])
   expect_identical(duplicates(as.data.frame(tib)), tib[c(1, 3), ])
@@ -7,8 +7,26 @@ test_that("can read multiple hobo csv file", {
     duplicates(data.frame(x = c(1, 2, 1), y = 1:3), "x"),
     dplyr::tibble(x = c(1, 1), y = c(1, 3))
   )
+  
   expect_equal(
     duplicates(data.frame(x = c(1, 2, 1), y = 1:3), c("x", "y")),
     dplyr::tibble(x = double(0), y = double(0))
   )
+})
+
+test_that("errors when no input argument is supplied", {
+  expect_error(
+    duplicates(),
+    'argument "x" is missing, with no default',
+    fixed = TRUE
+  )
+})
+
+test_that("errors when input argument is not a data.frame", {
+  expect_error(duplicates("a"), "Data.frame must be a data.frame.")
+  expect_error(duplicates(1), "Data.frame must be a data.frame.")
+  expect_error(duplicates(1L), "Data.frame must be a data.frame.")
+  expect_error(duplicates(TRUE), "Data.frame must be a data.frame.")
+  expect_error(duplicates(NULL), "Data.frame must be a data.frame.")
+  expect_error(duplicates(NA), "Data.frame must be a data.frame.")
 })

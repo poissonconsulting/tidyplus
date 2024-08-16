83d281ca5e357f3ace2be38d9a149837d58cac9b
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-08-16 15:52:52 -0600

    simplified dealing with sf tables with `duplicates()`

--- R/duplicates.R
+++ R/duplicates.R
@@ -21,57 +21,30 @@ duplicates <- function(.data, ..., .keep_all = TRUE) {
   chk_flag(.keep_all)
 
   col <- rlang::ensyms(...)
-  if (length(col) == 0) {
-    col_names <- colnames(.data)
-  } else {
-    col_names <- vapply(col, rlang::as_string, character(1))
-  }
+  if (length(col) == 0) { col_names <- colnames(.data) }
+  else { col_names <- vapply(col, rlang::as_string, character(1)) }
   col_names <- unique(col_names)
-  
   chk_vector(col_names)
   check_values(col_names, "")
   check_names(.data, col_names)
 
-  if (!length(col_names)) {
-    return(.data)
-  }
+  if (!length(col_names)) { return(.data) }
   
   grouped <- dplyr::is_grouped_df(.data)
   groups <- dplyr::group_vars(.data)
   groups_sym <- rlang::syms(groups)
   
+  is_sf = any(class(.data) == "sf")
+  
+  .data <- tibble::as_tibble(.data)
+  
   .data_dup <- dplyr::select(.data, dplyr::all_of(col_names))
   .data_dup <- .data_dup[duplicated(.data_dup), , drop = FALSE]
   .data_dup <- unique(.data_dup)
   
-  is_sf = any(class(.data) == "sf")
-  if(is_sf)
-  {
-    .data <- poisspatial::ps_sfc_to_coords(.data)
-    .data_dup <- poisspatial::ps_sfc_to_coords(.data_dup)
-    if (any(col_names == "geometry")) {
-      col_names <- col_names[col_names != "geometry"]
-      col_names <- c(col_names, "X", "Y")
-    }
-  }
-  
   .data <- dplyr::inner_join(.data, .data_dup, by = col_names) 
-  if (!(.keep_all)) {
-    .data <- dplyr::select(.data, dplyr::all_of(col_names))
-  }
-  
-  if(is_sf) {
-    .data <- poisspatial::ps_coords_to_sfc(.data)
-  }
-  
-  .data <- dplyr::as_tibble(.data)
-  
-  if(is_sf) {
-    .data <- poisspatial::ps_activate_sfc(.data)
-  }
-  
-  if (grouped) {
-      .data <- dplyr::group_by(.data, !!!groups_sym)
-  }
+  if (!(.keep_all)) { .data <- dplyr::select(.data, dplyr::all_of(col_names)) }
+  if (grouped) { .data <- dplyr::group_by(.data, !!!groups_sym) }
+  if (is_sf) { .data <- poisspatial::ps_activate_sfc(.data) }
   .data
 }

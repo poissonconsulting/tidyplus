3979a673a500f011bc8f152fc1f7f6389a3529b8
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-08-20 00:04:13 -0600

    formatting

--- R/duplicates.R
+++ R/duplicates.R
@@ -21,20 +21,25 @@ duplicates <- function(.data, ..., .keep_all = TRUE) {
   chk_flag(.keep_all)
 
   col <- rlang::ensyms(...)
-  if (length(col) == 0) { col_names <- colnames(.data) }
-  else { col_names <- vapply(col, rlang::as_string, character(1)) }
+  if (length(col) == 0) {
+    col_names <- colnames(.data)
+  } else {
+      col_names <- vapply(col, rlang::as_string, character(1))
+  }
   col_names <- unique(col_names)
   chk_vector(col_names)
   check_values(col_names, "")
   check_names(.data, col_names)
 
-  if (!length(col_names)) { return(.data) }
+  if (!length(col_names)) {
+    return(.data)
+  }
   
   grouped <- dplyr::is_grouped_df(.data)
   groups <- dplyr::group_vars(.data)
   groups_sym <- rlang::syms(groups)
   
-  is_sf = any(class(.data) == "sf")
+  is_sf <- any(class(.data) == "sf")
   
   .data <- tibble::as_tibble(.data)
   
@@ -43,8 +48,14 @@ duplicates <- function(.data, ..., .keep_all = TRUE) {
   .data_dup <- unique(.data_dup)
   
   .data <- dplyr::inner_join(.data, .data_dup, by = col_names) 
-  if (!(.keep_all)) { .data <- dplyr::select(.data, dplyr::all_of(col_names)) }
-  if (grouped) { .data <- dplyr::group_by(.data, !!!groups_sym) }
-  if (is_sf) { .data <- poisspatial::ps_activate_sfc(.data) }
+  if (!(.keep_all)) {
+    .data <- dplyr::select(.data, dplyr::all_of(col_names))
+  }
+  if (grouped) {
+    .data <- dplyr::group_by(.data, !!!groups_sym)
+  }
+  if (is_sf) {
+    .data <- poisspatial::ps_activate_sfc(.data)
+  }
   .data
 }

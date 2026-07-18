9eb0c799868fd45b06c28f6155279e602b94ff8e
Ayla Pearson <ayla@poissonconsulting.ca>
2026-07-18 10:25:35 -0700

    remove styler

--- scripts/build.R
+++ scripts/build.R
@@ -1,10 +1,5 @@
 roxygen2md::roxygen2md()
 
-styler::style_pkg(
-  scope = "line_breaks",
-  filetype = c("R", "Rmd")
-)
-
 lintr::lint_package(
   linters = lintr::linters_with_defaults(
     line_length_linter = lintr::line_length_linter(1000),
